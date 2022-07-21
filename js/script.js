6a7862f540c76144877c870092e92d2288af4cc6
osipovark <osipovarkonst@gmail.com>
2022-07-21 22:11:40 +0300

    modified js/script.js: added event listener which triggers when "Add show" button in menu is clicked on and causes form for adding new shows to slide into the view

--- js/script.js
+++ js/script.js
@@ -5,10 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
 class App {
   init() {
+
+    /* On clicking "hamburger" menu icon */
     document.querySelector('.header__menu-icon')
       .addEventListener('click', (event) => {
         event.target.classList.toggle('_active');
         document.querySelector('.header__menu').classList.toggle('_active');
+      });
+
+    /* On clicking "Add show" button in menu */
+    document.getElementById('add-show-button')
+      .addEventListener('click', (event) => {
+        let buttonAdd = document.getElementById('submit-show-button');
+        let buttonChange = document.getElementById('change-show-button');
+        if (buttonAdd.classList.contains('hidden')) {
+          buttonAdd.classList.remove('hidden');
+        }
+        if (!buttonChange.classList.contains('hidden')) {
+          buttonChange.classList.add('hidden');
+        }
+        event.target.parentElement.classList.remove('_active');
+        document.querySelector('.header__menu-icon').classList.remove('_active');
+        document.querySelector('.add-show-form').classList.add('_active');
+        let title = document.getElementById('added-show-title');
+        let description = document.getElementById('added-show-description');
+        title.value = 'title';
+        description.value = 'text';
       });
   }
 };
